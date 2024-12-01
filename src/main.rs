6568e3e303dd6bcdc0f425396d46ce8ea0bf7b23
akaashagarwal <akaash_agarwal@outlook.com>
2024-12-01 12:50:40 +0000

    puzzle 2 solution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod puzzle_1;
+mod puzzle_2;
 
 use puzzle_1::solve_puzzle_1;
+use puzzle_2::solve_puzzle_2;
 
 fn main() {
     println!("Answer to puzzle 1: {}", solve_puzzle_1());
+    println!("Answer to puzzle 2: {}", solve_puzzle_2());
 }
---
+++ src/puzzle_2.rs
@@ -0,0 +1,31 @@
+use std::{collections::HashMap, fs::read_to_string};
+
+pub fn solve_puzzle_2() -> u64 {
+    let mut location_id_list_1: Vec<u64> = vec![];
+    let mut location_id_list_2_counts: HashMap<u64, u64> = HashMap::new();
+
+    let input_str =
+        read_to_string("src/input/puzzle_1.txt").expect("Failed to read the input file.");
+
+    let lines = input_str.split_terminator("\n").collect::<Vec<&str>>();
+    for line in lines {
+        let id_pair = line
+            .split_ascii_whitespace()
+            .map(|id| id.parse::<u64>().unwrap())
+            .collect::<Vec<u64>>();
+        location_id_list_1.push(id_pair[0]);
+        if !location_id_list_2_counts.contains_key(&id_pair[1]) {
+            location_id_list_2_counts.insert(id_pair[1], 1);
+        } else {
+            location_id_list_2_counts
+                .entry(id_pair[1])
+                .and_modify(|counter| *counter += 1);
+        }
+    }
+
+    location_id_list_1
+        .into_iter()
+        .filter(|id| location_id_list_2_counts.contains_key(&id))
+        .map(|id| id * location_id_list_2_counts.get(&id).unwrap())
+        .sum()
+}
